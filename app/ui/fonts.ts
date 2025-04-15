615c547b758f3260514f578ceae09cce11ea7042
senmu <wb_qinxiaosen@meituan.com>
2025-04-15 18:23:22 +0800

    chore(learn): chapter 3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'], display: 'swap' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400', display: 'swap' });
